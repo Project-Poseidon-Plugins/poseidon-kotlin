1b9c43a2f1420e6357ed99b5d75fe1d1970b3b46
zavdav <157126752+zavdav@users.noreply.github.com>
2024-12-22 13:21:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# poseidon-kotlin
+
+Provides the kotlin-stdlib-jdk8 as a plugin for plugins written in Kotlin to use.
---
+++ src/main/kotlin/org/poseidonplugins/kotlin/PoseidonKotlin.kt
@@ -0,0 +1,10 @@
+package org.poseidonplugins.kotlin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PoseidonKotlin : JavaPlugin() {
+
+    override fun onEnable() {}
+
+    override fun onDisable() {}
+}
